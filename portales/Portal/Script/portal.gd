e799cc358491bd051028b62227c818e207a92d25
Ricardo Román <ricardo.roman@ug.uchile.cl>
2025-09-27 15:34:26 -0300

    Feat: Creacion de nodo Portal de Salida

--- portales/Portal/Script/portal.gd
+++ portales/Portal/Script/portal.gd
@@ -1,1 +1,4 @@
 extends Node2D
+
+func _ready() -> void:
+	print(get_tree().get_nodes_in_group("portalSalida").size())
---
+++ portales/Portal/Script/portal_salida.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+func _ready() -> void:
+	print(get_tree().get_nodes_in_group("portalEntrada").size())
