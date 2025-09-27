538c4ef5ea5f3859e20a2b585e189f691e4dc3ef
Ricardo Román <ricardo.roman@ug.uchile.cl>
2025-09-27 15:58:27 -0300

    Feat: Implementacion de la funcionalidad de los portales

--- portales/Portal/Script/portal.gd
+++ portales/Portal/Script/portal.gd
@@ -1,4 +1,17 @@
 extends Node2D
 
+var linkPortal: Vector2
+
 func _ready() -> void:
-	print(get_tree().get_nodes_in_group("portalSalida").size())
+	var entrada = get_tree().get_nodes_in_group("portalEntrada")
+	var salida = get_tree().get_nodes_in_group("portalSalida")
+	
+	for i in range (entrada.size()):
+		print("Linkeado portal ", i + 1, "Posiciones: ", entrada[i].position, " y ",salida[i].position)
+		linkPortal = salida[i].position
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.get_parent().is_in_group("player"):
+		print("Jugador entro al portal")
+		area.get_parent().position = linkPortal
