fe3cf4603bd2ae95f79ed67441f1960ba355eac0
Ricardo Román <ricardo.roman@ug.uchile.cl>
2025-10-25 18:37:59 -0300

    Fix: Resolve issue when linking multiple portals simultaneously

--- portales/Portal/Script/portal_salida.gd
+++ portales/Portal/Script/portal_salida.gd
@@ -7,5 +7,5 @@ func _ready() -> void:
 	var salida = get_tree().get_nodes_in_group("portalSalida")
 	
 	for i in range (salida.size()):
-		print("Linkeado portal ", i + 1, "Posiciones: ", salida[i].position, " y ",entrada[i].position)
+		print("Linkeado portal salida ", i + 1, " Posiciones: ", salida[i].position, " y ",entrada[i].position)
 		linkPortal = entrada[i].position
