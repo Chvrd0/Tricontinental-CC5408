7944fe5721a44d2270638dc0ecb98a1c878f7938
nicolasasseo <nicolasasseogithub@gmail.com>
2025-12-02 15:58:30 -0300

    Credits -> Menu & removed corrupted file

--- portales/menus/credits.gd
+++ portales/menus/credits.gd
@@ -10,3 +10,13 @@ func _ready():
 
 func _on_credits_pressed(): 
 	LevelManager.go_to_main_menu()
+	
+
+func _process(delta):
+	# Check for "Enter" (ui_accept) OR "Escape" (pause)
+	if Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("pause"):
+		_return_to_menu()
+
+# I renamed this function to be more generic since it's used by keys AND mouse now
+func _return_to_menu():
+	LevelManager.go_to_main_menu()
